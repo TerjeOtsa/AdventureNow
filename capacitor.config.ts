a84e22be823b3d57cfcb27a9030b24a290e8c89f
TerjeOtsa <71086730+TerjeOtsa@users.noreply.github.com>
2024-05-26 22:38:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AdventureNow',
+  webDir: 'www'
+};
+
+export default config;
